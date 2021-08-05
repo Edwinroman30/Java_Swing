8f6c7782d9e130288153e8f732fa04829b728396
Edwin Roman <romanseberino@gmail.com>
2021-08-05 11:45:19 -0400

    Estado de entrega favorable para el profesor

--- Soto_practices/Segundo_Parcial/src/CalculadorPrestaciones.java
+++ Soto_practices/Segundo_Parcial/src/CalculadorPrestaciones.java
@@ -262,14 +262,7 @@ public class CalculadorPrestaciones {
     
     public void calculoRegaliaPascual(){
         
-        //SI los dias no pasan de 15 días para no abusar.
-        if(empleado.getFechaSalida()[0] < 15)
-        {
-            this.regaliaPascual = ((empleado.getSalario() * (empleado.getFechaSalida()[1] - 1)) / 12);
-        }
-        else{
-            this.regaliaPascual = ((empleado.getSalario() * (empleado.getFechaSalida()[1])) / 12);
-        }
+        this.regaliaPascual = ((empleado.getSalario() * (empleado.getFechaSalida()[1])) / 12);
     }
 
     
